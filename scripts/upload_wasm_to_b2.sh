f058697287fdedbd9bf026a5a8fd7dfe375e762b
Billy Zhu <zyx.billy@gmail.com>
2022-06-11 23:15:33 -0700

    use sync for non-wasm files

--- scripts/upload_wasm_to_b2.sh
+++ scripts/upload_wasm_to_b2.sh
@@ -20,12 +20,10 @@ pip install b2
 # Authenticate with b2
 b2 authorize-account $B2_APP_KEY_ID $B2_APP_KEY
 
-# Upload data files in public/wasm
-for datafile in *.data; do
-    b2 upload-file $B2_BUCKET $datafile $REMOTE_DIR/$datafile
-done
+# Upload data files using `b2 sync` to save uploading the same file.
+b2 sync --excludeRegex '.*\.wasm' --replaceNewer --noProgress ./ b2://$REMOTE_DIR
 
-# Upload wasm files in public/wasm
+# Upload wasm files individually to set custom contentType.
 for wasmfile in *.wasm; do
     b2 upload-file $B2_BUCKET $wasmfile $REMOTE_DIR/$wasmfile --contentType application/wasm
 done
