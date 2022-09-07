79565ddaac46c739cdc1b464a04e57cb5d3129aa
Billy Zhu <zyx.billy@gmail.com>
2022-09-07 00:07:44 -0700

    add plausible provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <PlausibleProvider domain="playground.mlir-china.org">
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </PlausibleProvider>
   );
 }
 
