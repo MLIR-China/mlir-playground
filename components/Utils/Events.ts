3143652d523ed0bee6b79c19e4f836f87508079b
Billy Zhu <zyx.billy@gmail.com>
2022-09-07 18:47:29 -0700

    [log] Integrate Plausible (#66)
    * add dependency
    
    * add plausible provider
    
    * add basic logs

---
+++ components/Utils/Events.ts
@@ -0,0 +1,10 @@
+type EnvDownloadStart = { kind: "EnvDownloadStart"; props: never };
+type EnvDownloadDone = { kind: "EnvDownloadDone"; props: { success: boolean } };
+type RunStart = { kind: "RunStart"; props: { preset: string } };
+type RunEnd = { kind: "RunEnd"; props: never };
+
+type PlaygroundEvent = EnvDownloadStart | EnvDownloadDone | RunStart | RunEnd;
+
+export type AllPlaygroundEvents = {
+  [EventKind in PlaygroundEvent as EventKind["kind"]]: EventKind["props"];
+};
