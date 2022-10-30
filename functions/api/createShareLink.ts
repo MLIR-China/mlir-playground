1267e3ba6605624e5ca421bfaff8d4a53cb8ce43
Billy Zhu <zyx.billy@gmail.com>
2022-10-29 19:36:22 -0700

    fix api logic for functions

--- functions/api/createShareLink.ts
+++ functions/api/createShareLink.ts
@@ -65,8 +65,16 @@ async function handleRequest(request: Request, env: any) {
   );
 }
 
-export default {
-  async fetch(request: Request, env: any) {
-    return await handleRequest(request, env);
-  },
-};
+export async function onRequest(context: any) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+
+  return handleRequest(request, env);
+}
