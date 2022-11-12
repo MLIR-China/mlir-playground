ae1f14e17b034d0ca7a9e06e8826e071baad293e
Billy Zhu <zyx.billy@gmail.com>
2022-11-12 15:53:51 -0800

    handle cors preflight request

--- functions/api/createShareLink.ts
+++ functions/api/createShareLink.ts
@@ -1,5 +1,11 @@
 import { validateAgainstSchema } from "../../schema/validation";
 
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+};
+
 const USER_CONTENT_SIZE_LIMIT = 128 * 2 ** 10; // 128KB
 
 function createUserErrorResponse(message: string) {
@@ -67,8 +73,46 @@ async function handleRequest(request: Request, env: any) {
   return new Response(
     JSON.stringify({
       resource: env.UGC_URL_PREFIX + filename,
-    })
+    }),
+    {
+      headers: CORS_HEADERS,
+    }
   );
+}
+
+function handlePreflightOptions(request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers;
+  if (
+    headers.get("Origin") !== null &&
+    headers.get("Access-Control-Request-Method") !== null &&
+    headers.get("Access-Control-Request-Headers") !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    let respHeaders = {
+      ...CORS_HEADERS,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      "Access-Control-Allow-Headers": request.headers.get(
+        "Access-Control-Request-Headers"
+      ),
+    };
+
+    return new Response(null, {
+      headers: respHeaders,
+    });
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: "GET, HEAD, POST, OPTIONS",
+      },
+    });
+  }
 }
 
 export async function onRequest(context: any) {
@@ -81,6 +125,10 @@ export async function onRequest(context: any) {
     next, // used for middleware or to fetch assets
     data, // arbitrary space for passing data between middlewares
   } = context;
+
+  if (request.method === "OPTIONS") {
+    return handlePreflightOptions(request);
+  }
 
   return handleRequest(request, env);
 }
