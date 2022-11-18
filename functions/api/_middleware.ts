4436c11a738e24104b05f1089562d58a4e7e8cf0
Billy Zhu <zyx.billy@gmail.com>
2022-11-17 22:40:25 -0800

    use middleware for cors

---
+++ functions/api/_middleware.ts
@@ -0,0 +1,24 @@
+/**
+ * Cors headers: https://developers.cloudflare.com/pages/platform/functions/examples/cors-headers/
+ */
+
+// Respond to OPTIONS method
+export const onRequestOptions: PagesFunction = async () => {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+};
+
+// Set CORS to all /api responses
+export const onRequest: PagesFunction = async ({ next }) => {
+  const response = await next();
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Max-Age", "86400");
+  return response;
+};
