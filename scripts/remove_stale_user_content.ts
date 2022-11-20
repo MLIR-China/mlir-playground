ede0b8190af1c402920a51ad569b41c7c08cc56c
Billy Zhu <zyx.billy@gmail.com>
2022-11-19 22:45:53 -0800

    format

--- scripts/remove_stale_user_content.ts
+++ scripts/remove_stale_user_content.ts
@@ -6,7 +6,7 @@
 
 const R2_ENDPOINT = "<FILL IN R2 ENDPOINT HERE>";
 const BUCKET_NAME = "mlir-playground-ugc";
-const STALE_TIME_THRESHOLD = 48 * 60 * 60 * 1000 // 48 hours in milliseconds
+const STALE_TIME_THRESHOLD = 48 * 60 * 60 * 1000; // 48 hours in milliseconds
 
 import {
   S3Client,
@@ -28,21 +28,28 @@ async function removeStaleUserContents(staleBeforeTime: number) {
   // Find all stale objects' keys.
   let truncated = true;
   let listObjectsParams: ListObjectsV2CommandInput = {
-    Bucket: BUCKET_NAME
+    Bucket: BUCKET_NAME,
   };
   while (truncated) {
     try {
-      const listResponse = await client.send(new ListObjectsV2Command(listObjectsParams));
+      const listResponse = await client.send(
+        new ListObjectsV2Command(listObjectsParams)
+      );
 
       listResponse.Contents?.forEach((item) => {
-        if (item.LastModified && item.Key && item.LastModified.getTime() < staleBeforeTime) {
+        if (
+          item.Key &&
+          item.LastModified &&
+          item.LastModified.getTime() < staleBeforeTime
+        ) {
           staleObjectKeys.push(item.Key);
         }
-      })
+      });
 
       truncated = !!listResponse.IsTruncated;
       if (truncated) {
-        listObjectsParams.ContinuationToken = listResponse.NextContinuationToken;
+        listObjectsParams.ContinuationToken =
+          listResponse.NextContinuationToken;
       }
     } catch (error) {
       console.log("Failed to list objects: " + String(error));
@@ -55,17 +62,21 @@ async function removeStaleUserContents(staleBeforeTime: number) {
   // Delete stale keys from the bucket.
   while (staleObjectKeys.length > 0) {
     // Get the first 1000 keys, and remove them from the original list.
-    const currentBatchObjects: Array<ObjectIdentifier> = staleObjectKeys.splice(0, 1000).map((key) => ({Key: key}));
+    const currentBatchObjects: Array<ObjectIdentifier> = staleObjectKeys
+      .splice(0, 1000)
+      .map((key) => ({ Key: key }));
     const deleteObjectsParams: DeleteObjectsCommandInput = {
       Bucket: BUCKET_NAME,
       Delete: {
         Objects: currentBatchObjects,
-        Quiet: true
-      }
+        Quiet: true,
+      },
     };
 
     try {
-      const deleteResponse = await client.send(new DeleteObjectsCommand(deleteObjectsParams));
+      const deleteResponse = await client.send(
+        new DeleteObjectsCommand(deleteObjectsParams)
+      );
 
       if (deleteResponse.Errors && deleteResponse.Errors.length > 0) {
         console.log("Delete request returned errors:");
