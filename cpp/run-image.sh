0b7865968b0b631fecfe0f343984bcd805c68f60
Billy Zhu <zyx.billy@gmail.com>
2023-04-10 05:38:08 +0000

    make run-image take image as arg

--- cpp/run-image.sh
+++ cpp/run-image.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run --rm -ti -w /app/ -v $(pwd):/app/ clang-wasm bash
+
+# Command line argument to override docker image. Default is `clang-wasm`.
+image=${1:-clang-wasm:latest}
+
+docker run --rm -ti -w /app/ -v $(pwd):/app/ $image bash
